1a4150d54bce2bdb24270905761cae93e860aeec
Matthew Clarke <m_d_clarke@hotmail.com>
2023-08-14 10:38:12 +0200

    added completed scheduler

--- server.js
+++ server.js
@@ -126,3 +126,119 @@ app.post("/data/event-update/:id", async (req, res) => {
 app.listen(port, () => {
   console.log(`Started on localhost: ${port}`);
 });
+
+app.get("/load", async (req, res) => {
+  try {
+    const [[resources], [events]] = await Promise.all([
+      db.query("SELECT * FROM bryntum_scheduler_resources"),
+      db.query("SELECT * FROM bryntum_scheduler_events"),
+    ]);
+    res.send({
+      success: true,
+      resources: {
+        rows: resources,
+      },
+      events: {
+        rows: events,
+      },
+    });
+  } catch (error) {
+    console.error({ error });
+    res.send({
+      success: false,
+      message: "There was an error loading the resources and events data.",
+    });
+  }
+});
+
+// create, update, and delete Bryntum Scheduler events
+app.post("/sync", async function (req, res) {
+  const { requestId, resources, events } = req.body;
+  try {
+    const response = { requestId, success: true };
+    if (resources) {
+      const rows = await applyTableChanges(
+        "bryntum_scheduler_resources",
+        resources
+      );
+      // if new data to update client
+      if (rows) {
+        response.resources = { rows };
+      }
+    }
+    if (events) {
+      const rows = await applyTableChanges("bryntum_scheduler_events", events);
+      // if new data to update client
+      if (rows) {
+        response.events = { rows };
+      }
+    }
+    res.send(response);
+  } catch (error) {
+    console.error({ error });
+    res.send({
+      requestId,
+      success: false,
+      message: "There was an error syncing the data.",
+    });
+  }
+});
+
+async function applyTableChanges(table, changes) {
+  let rows;
+  if (changes.added) {
+    rows = await createOperation(changes.added, table);
+  }
+  if (changes.removed) {
+    await deleteOperation(changes.removed, table);
+  }
+  if (changes.updated) {
+    await updateOperation(changes.updated, table);
+  }
+  // if got some new data to update client
+  return rows;
+}
+
+function createOperation(added, table) {
+  return Promise.all(
+    added.map(async (record) => {
+      const { $PhantomId, exceptionDates, ...data } = record;
+      // insert record
+      const [result] = await db.query("INSERT INTO ?? set ?", [
+        table,
+        table === "bryntum_scheduler_resources"
+          ? data
+          : {
+              ...data,
+              exceptionDates: JSON.stringify(exceptionDates),
+            },
+      ]);
+      // report to the client that we changed the record identifier
+      return { $PhantomId, id: result.insertId };
+    })
+  );
+}
+
+function deleteOperation(deleted, table) {
+  return db.query(
+    `DELETE FROM ${table} WHERE id in (?)`,
+    deleted.map(({ id }) => id)
+  );
+}
+
+function updateOperation(updated, table) {
+  return Promise.all(
+    updated.map(({ id, exceptionDates, ...data }) => {
+      return db.query("UPDATE ?? set ? where id = ?", [
+        table,
+        table === "bryntum_scheduler_resources"
+          ? data
+          : {
+              ...data,
+              exceptionDates: JSON.stringify(exceptionDates),
+            },
+        id,
+      ]);
+    })
+  );
+}
